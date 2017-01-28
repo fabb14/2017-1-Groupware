878acc95a1f1f6d26ce178b01f4f5a885622f9a7
laboratorio <laboratorio@LAB09-14>
2017-01-28 16:36:04 -0600

    Se agrega proyecto de Lógica de Negocio para la calculadora

---
+++ ULatina.Topicos.Calentamiento/ULatina.Topicos.Calculadora/Dominio/Acciones/Multiplicar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ULatina.Topicos.Calculadora.Dominio.Acciones
+{
+    public class Multiplicar
+    {
+    }
+}
